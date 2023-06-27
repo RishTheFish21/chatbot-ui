f4069726e19d5f572641d11e950d432ddbe27d25
RishTheFish21 <rishadzzzz@gmail.com>
2023-06-27 18:23:00 -0400

    Update File

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -354,21 +354,6 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
           <div className="text-center text-4xl font-bold text-black dark:text-white">
             Welcome to Chatbot UI
           </div>
-          <div className="text-center text-lg text-black dark:text-white">
-            <div className="mb-8">{`Chatbot UI is an open source clone of OpenAI's ChatGPT UI.`}</div>
-            <div className="mb-2 font-bold">
-              Important: Chatbot UI is 100% unaffiliated with OpenAI.
-            </div>
-          </div>
-          <div className="text-center text-gray-500 dark:text-gray-400">
-            <div className="mb-2">
-              Chatbot UI allows you to plug in your API key to use this UI with
-              their API.
-            </div>
-            <div className="mb-2">
-              It is <span className="italic">only</span> used to communicate
-              with their API.
-            </div>
             <div className="mb-2">
               {t(
                 'Please set your OpenAI API key in the bottom left of the sidebar.',
